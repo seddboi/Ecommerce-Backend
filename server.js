67e5d429b3b9404a29718e5ff16e54d2eb0fb6fe
Gian Zamora <gz2016@att.net>
2021-05-12 22:29:50 -0700

    adjusted missing code which syncs the models to the database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { sequelize } = require('./models/Product');
 const routes = require('./routes');
 // import sequelize connection
 
@@ -11,9 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({forve: false}).then( () => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
+
 
 
